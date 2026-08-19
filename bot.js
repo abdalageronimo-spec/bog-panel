d53f4f48b6964e2da0c42c62d113c42e3f258eaa
abdalageronimo-spec <abdalageronimo@gmail.com>
2026-08-19 12:20:36 -0500

    Change Telegram bot token in bot.js
    Updated Telegram bot token for authentication.

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '8739783518:AAFf-Mwey3kYHSGaMOf-E2YgVHOox0sKEQ8';
+const token = '8739783518:AAFcTJUeh1CRrBRn8_MJHbhSk6AvCC9pBJk';
 
 if (!token) {
     console.error('❌ TELEGRAM_TOKEN no está definido');
     process.exit(1);
 }
 
-const bot = new TelegramBot('8739783518:AAFf-Mwey3kYHSGaMOf-E2YgVHOox0sKEQ8', {
+const bot = new TelegramBot('8739783518:AAFcTJUeh1CRrBRn8_MJHbhSk6AvCC9pBJk', {
     polling: true
 });
 
